bcea08c71d24d09925bd9a2f316d89909b2a18e1
LuisaFC <luisabsb@gmail.com>
2025-02-24 14:54:05 -0300

    Reorder component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { useFieldArray, useForm } from "react-hook-form";
+import { Reorder } from "framer-motion";
+import { useState } from "react";
+import { cn } from "./lib/utils";
 
 export default function App() {
   const form = useForm({
@@ -25,6 +28,24 @@ export default function App() {
     name: 'links',
   })
 
+  const [dragginIndex, setDragginIndex] = useState<null | number>(null)
+
+  const handleSubmit = form.handleSubmit((data) => {
+    console.log(data)
+  })
+
+  function hanldeDragStart(index: number){
+    setDragginIndex(index)
+  }
+
+  function handleDragEnd(){
+    setDragginIndex(null)
+  }
+
+  function handleReorder(){
+
+  }
+
   return (
     <div className="grid place-items-center min-h-screen">
       <div className="w-full max-w-2xl">
@@ -40,32 +61,50 @@ export default function App() {
             Adicionar no topo da lista
           </Button>
 
-        <form className=" flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className=" flex flex-col gap-4">
 
-          {links.fields.map((link, index) => (
-             <div key={link.id}className="flex gap-4">
-                <div className="flex-1 space-y-2">
-                  <Label htmlFor="title">Títulos</Label>
-                  <Input id="title" {...form.register(`links.${index}.title`)}/>
-                </div>
-    
-                <div className="flex-1 flex gap-4 items-end">
+          <Reorder.Group
+            axis="y" //eixo
+            values={links.fields} //array de valores
+            onReorder={handleReorder}
+          >
+            {links.fields.map((link, index) => (
+              <Reorder.Item
+                key={link.id}
+                value={link}
+                onDragStart={() => hanldeDragStart(index)}
+                onDragEnd={handleDragEnd}
+              >
+                <div 
+                  className={cn(
+                    "flex gap-4 transition-opacity",
+                    dragginIndex !== null && dragginIndex !== index && 'opacity-50'
+                  )}
+                >
                   <div className="flex-1 space-y-2">
-                    <Label htmlFor="url">URL</Label>
-                    <Input id="url" {...form.register(`links.${index}.url`)}/>
+                    <Label htmlFor="title">Títulos</Label>
+                    <Input id="title" {...form.register(`links.${index}.title`)}/>
                   </div>
-    
-                  <Button 
-                    type="button" 
-                    variant='destructive'
-                    onClick={() => links.remove(index)}
-                    tabIndex={-1}
-                  >
-                    <Trash2Icon className="size-4"/>
-                  </Button>
+      
+                  <div className="flex-1 flex gap-4 items-end">
+                    <div className="flex-1 space-y-2">
+                      <Label htmlFor="url">URL</Label>
+                      <Input id="url" {...form.register(`links.${index}.url`)}/>
+                    </div>
+      
+                    <Button 
+                      type="button" 
+                      variant='destructive'
+                      onClick={() => links.remove(index)}
+                      tabIndex={-1}
+                    >
+                      <Trash2Icon className="size-4"/>
+                    </Button>
+                  </div>
                 </div>
-              </div>
-          ))}
+              </Reorder.Item>
+            ))}
+          </Reorder.Group>
 
           <Button 
             type="button"
@@ -123,6 +162,10 @@ export default function App() {
               Update
             </Button>
           </div>
+
+          <Button type="submit" className="w-full mt-6">
+            Salvar
+          </Button>
         </form>
       </div>
     </div>
